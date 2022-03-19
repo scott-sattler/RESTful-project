1b8dbf45028ff55524b84ea5cd0b203be07bccfc
scott-sattler <59404643+scott-sattler@users.noreply.github.com>
2022-03-18 20:53:30 -0500

    fixed path; structure dependency

--- microservices/run_servers.bat
+++ microservices/run_servers.bat
@@ -1,4 +1,5 @@
 @echo off
-start cmd.exe /c ""C:\Users\Computer\github\microservices\gateway\venv\Scripts\python.exe" "C:\Users\Computer\github\microservices\gateway\main.py""
-start cmd.exe /c ""C:\Users\Computer\github\microservices\frontend\venv\Scripts\python.exe" "C:\Users\Computer\github\microservices\frontend\main.py""
-start cmd.exe /c ""C:\Users\Computer\github\microservices\backend\color_lookup\venv\Scripts\python.exe" "C:\Users\Computer\github\microservices\backend\color_lookup\main.py""
+
+start cmd.exe /c ""%~dp0\gateway\venv\Scripts\python.exe" "%~dp0\gateway\main.py""
+start cmd.exe /c ""%~dp0\frontend\venv\Scripts\python.exe" "%~dp0\frontend\main.py""
+start cmd.exe /c ""%~dp0\backend\color_lookup\venv\Scripts\python.exe" "%~dp0\backend\color_lookup\main.py""
